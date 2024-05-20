05679efd43b346480030eaf08eee8ef5526f8203
Hermann EHO <hermannleboss@gmail.com>
2024-05-20 23:59:25 +0000

    chore: firebase config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,30 @@
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@vueuse/nuxt', "@nuxtjs/tailwindcss"],
-})
+  modules: ["@vueuse/nuxt", "@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    // Will be available in both client and server
+    firebase: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID,
+    },
+    public: {
+      firebase: {
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DA,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID,
+      },
+    },
+  },
+});
