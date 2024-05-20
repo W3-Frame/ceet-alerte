427f2c4bf0f1474d99c7a16b6dcc911349eb9e4d
Hermann EHO <hermannleboss@gmail.com>
2024-05-20 23:59:38 +0000

    feat: login with google

--- pages/login.vue
+++ pages/login.vue
@@ -2,28 +2,37 @@
   <div>
     <div class="card w-96 bg-base-100 shadow-xl">
       <div class="card-body">
-        
-      <p class="text-2xl text-gray-900 dark:text-white font-bold">
-        Welcome back
-      </p>
-      <input type="text" placeholder="Enter Your Email" class="input input-bordered w-full max-w-xs" />
-      <input type="text" placeholder="Enter Your Password" class="input input-bordered w-full max-w-xs" />
-      <button class="p-2 btn btn-neutral">Submit</button>
-      
-      <div class="flex items-center align-center text-center w-full flex-row">
-        <div
-          class="flex border-gray-200 dark:border-gray-800 w-full border-t border-solid"
-        ></div>
-        <div
-          class="font-medium text-gray-700 dark:text-gray-200 flex mx-3 whitespace-nowrap"
-        >
-          <span class="text-sm">or</span>
+        <p class="text-2xl text-gray-900 dark:text-white font-bold">
+          Welcome back
+        </p>
+        <input
+          type="text"
+          placeholder="Enter Your Email"
+          class="input input-bordered w-full max-w-xs"
+        />
+        <input
+          type="text"
+          placeholder="Enter Your Password"
+          class="input input-bordered w-full max-w-xs"
+        />
+        <button class="p-2 btn btn-neutral">Submit</button>
+
+        <div class="flex items-center align-center text-center w-full flex-row">
+          <div
+            class="flex border-gray-200 dark:border-gray-800 w-full border-t border-solid"
+          ></div>
+          <div
+            class="font-medium text-gray-700 dark:text-gray-200 flex mx-3 whitespace-nowrap"
+          >
+            <span class="text-sm">or</span>
+          </div>
+          <div
+            class="flex border-gray-200 dark:border-gray-800 w-full border-t border-solid"
+          ></div>
         </div>
-        <div
-          class="flex border-gray-200 dark:border-gray-800 w-full border-t border-solid"
-        ></div>
-      </div>
-      <button class="p-2 btn btn-secondary  btn-outline">Continue With Google</button>
+        <button class="p-2 btn btn-secondary btn-outline" @click="signIn()">
+          Continue With Google
+        </button>
         <!-- <h2 class="card-title">Shoes!</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div class="card-actions justify-end">
@@ -35,9 +44,23 @@
 </template>
 
 <script setup lang="ts">
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useAuth } from "@vueuse/firebase/useAuth";
+import { useFirebase } from "~/composables/firebase";
 definePageMeta({
   layout: "auth",
 });
+
+const { auth } = useFirebase();
+const { isAuthenticated, user } = useAuth(auth);
+if (isAuthenticated) {
+  navigateTo("/");
+}
+
+const signIn = () =>
+  signInWithPopup(auth, new GoogleAuthProvider()).then(() => {
+    navigateTo("/");
+  });
 </script>
 
 <style scoped></style>
