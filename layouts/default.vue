0ecb50b3e4375096cbdb1cb718c91063db9c83aa
Hermann EHO <hermannleboss@gmail.com>
2024-05-21 09:08:21 +0000

    style: :art: use navbar component in the default layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class: "dark:bg-gray-950",
+  },
+});
+</script>
+
+<template>
+  <div class="min-h-screen max-w-screen-lg mx-auto">
+    <NavBar />
+
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
