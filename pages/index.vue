9233e14ae643b91b7b7ba9d90a285a73b7fd5548
Hermann EHO <hermannleboss@gmail.com>
2024-05-21 09:11:56 +0000

    refactor: home page using default layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>Home Page</div>
+  <div class="flex flex-row h-96">
+    <div class="min-h-full">
+      <ul class="menu bg-base-200 w-56 rounded-box">
+        <li><a>Zone 1</a></li>
+        <li><a>Zone 2</a></li>
+        <li><a>Zone 3</a></li>
+      </ul>
+    </div>
+
+    <div class="divider divider-horizontal"></div>
+    <div class="w-full h-20 card bg-base-300 rounded-box place-items-center">
+      content
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+definePageMeta({
+  layout: "default",
+});
+</script>
 
-<style scoped></style>
+<style scoped>
+</style>
